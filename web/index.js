72e0fbf4a4fc45d454c8614e0e59f3176fb9e778
Markus Voigt <markus.voigt90@gmail.com>
2023-01-14 12:41:34 +0100

    headless test endpoint

--- web/index.js
+++ web/index.js
@@ -535,22 +535,21 @@ app.get("/headlessdata/:customerAccesstoken", async (_req, res) => {
     return;
   }
   const session = await getSessionForShop();
-  const allMetaFields = await getMetafieldDefinitionsForShop();
-  const customerMetaFields = await getMetafieldsForCustomer(
+  const allMetafields = await getMetafieldDefinitionsForShop();
+  const customerMetafields = await getMetafieldsForCustomer(
     response.body.data.customer.id,
     session
   );
-  // include all definitions for metafields not currently set for the customer object
-  for (let metafield of allMetaFields) {
-    if (!customerMetaFields.find((m) => m.key == metafield.key)) {
-      //customerMetaFields.push(metafield);
-    }
+
+  for (let customerMetafield of customerMetafields) {
+    const definition = allMetafields.find(
+      (m) => m.key == customerMetafield.key
+    );
+    definition.value = customerMetafield.value;
+    definition.customerMetafieldID = customerMetafield.id;
   }
-  const info = {
-    customerMetaFields,
-    allMetaFields,
-  };
-  res.status(200).send(info);
+
+  res.status(200).send(allMetafields);
 });
 
 async function getStorefrontClientForShop(shop = "markusvoigt.myshopify.com") {
