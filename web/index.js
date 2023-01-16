78bb35b4cfc6f9f0b03825b96f3b3351fb52cdc6
Markus Voigt <markus.voigt90@gmail.com>
2023-01-16 21:28:59 +0100

    error handling

--- web/index.js
+++ web/index.js
@@ -532,7 +532,7 @@ app.options("/metafields", (req, res) => {
 app.post("/metafields", async (_req, res) => {
   const customerAccessToken = _req.body.customerAccessToken;
   const storefrontAccessToken = _req.body.storefrontAccessToken;
-  const shop = _req.body.shop || "markusvoigt.myshopify.com";
+  const shop = _req.body.shop;
 
   const customerID = await validateCustomerID(
     customerAccessToken,
